750e30912c84482284be6d2095415405e25886ea
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-07-04 20:41:01 -0300

    Config do multer

--- Image_uploader/backend/index.ts
+++ Image_uploader/backend/index.ts
@@ -1,9 +1,15 @@
 import express from 'express'
 import multer from 'multer'
+import { storage } from './multerConfig'
+
+const upload = multer({storage: storage})
+
 
 const app = express()
 
-
+app.post('/upload', upload.single('file'), (req, res)=>{
+    return res.json(req.file.filename)
+})
 
 
 app.listen(3001)
