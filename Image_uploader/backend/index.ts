90b0775b826ee10203e530b8373aad176aa15815
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-07-04 20:54:04 -0300

    Finalizando funções principais

--- Image_uploader/backend/index.ts
+++ Image_uploader/backend/index.ts
@@ -7,8 +7,11 @@ const upload = multer({storage: storage})
 
 const app = express()
 
+app.use('/files', express.static('uploads'))
+
+
 app.post('/upload', upload.single('file'), (req, res)=>{
-    return res.json(req.file.filename)
+    return res.json(req.file?.filename)
 })
 
 
