98e7a7c4fd1d7d5e79ce768a59b3d7abd03dc254
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-07-11 19:39:43 -0300

    Testing connection with the backend

--- Image_uploader/backend/index.js
+++ Image_uploader/backend/index.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const multer = require('multer')
-const { storage } = require('./multerConfig')
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const { storage } = require("./multerConfig");
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage });
 
+const app = express();
+app.use(cors());
 
-const app = express()
+app.use("/files", express.static("uploads"));
 
-app.use('/files', express.static('uploads'))
-
-app.post('/upload', upload.single('file'), (req, res)=>{
-    return res.json(req.file?.filename)
+app.get('/', (req, res) =>{
+    res.json({message:'Backend conected'})
 })
 
+app.post("/upload", upload.single("file"), async (req, res) => {
+    res.json(req.file?.filename)
+});
 
-app.listen(3001)
+app.listen(3001);
